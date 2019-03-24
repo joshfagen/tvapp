0d78ccc76edbedc16950c4e1819894f55125383e
Josh Fagen <joshmfagen@gmail.com>
2019-03-24 15:45:52 -0400

    Added Material, created Shows and Show components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  (path: '**', component: NotFoundComponent),
+  (path: 'show/:id', component: ShowComponent),
+  (path: 'search/:query', component: ShowsComponent)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './views/navbar/navbar.component';
+
 import { HomeComponent } from './views/home/home.component';
+import { ShowsComponent } from './shows/shows.component';
+import { ShowComponent } from './show/show.component';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ShowsComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
